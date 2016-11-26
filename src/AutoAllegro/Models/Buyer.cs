ae3babf4a728fc27078d174ceb9927666ca57af0
Dominik Baran <dominik.baran7@gmail.com>
2016-11-26 18:30:17 +0100

    Dodano model Buyer i SendAddress.

---
+++ src/AutoAllegro/Models/Buyer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AutoAllegro.Models
+{
+    public class Buyer
+    {
+        public int Id { get; set; }
+        public string UserLogin { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PostCode { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Phone2 { get; set; }
+        public string UserBirthDate { get; set; }
+        public long AllegroUserId { get; set; }
+    }
+
+}
