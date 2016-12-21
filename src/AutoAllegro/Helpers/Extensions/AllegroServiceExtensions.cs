9a28ed2523e95dc25d7bb6d5087ed9c5fcb117cd
Dominik Baran <dominik.baran7@gmail.com>
2016-12-21 21:49:17 +0100

    Renamed class.

--- src/AutoAllegro/Helpers/Extensions/AllegroServiceExtensions.cs
+++ src/AutoAllegro/Helpers/Extensions/AllegroServiceExtensions.cs
@@ -2,7 +2,7 @@
 
 namespace AutoAllegro.Helpers.Extensions
 {
-    public static class AllegroServiceExtensions
+    public static class DateTimeExtensions
     {
         private static readonly DateTime UnixDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
         public static DateTime ToDateTime(this long unixTimeStamp)
