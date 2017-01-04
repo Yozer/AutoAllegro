15a14960d0fc580c4fb17f0d261d5c937724672c
Dominik Baran <dominik.baran7@gmail.com>
2017-01-04 20:45:37 +0100

    Added possibility to change Allegro settings.

--- src/AutoAllegro/Controllers/ManageController.cs
+++ src/AutoAllegro/Controllers/ManageController.cs
@@ -1,26 +1,32 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
+using AutoAllegro.Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using AutoAllegro.Models;
 using AutoAllegro.Models.ManageViewModels;
+using Microsoft.EntityFrameworkCore;
 
 namespace AutoAllegro.Controllers
 {
     [Authorize]
     public class ManageController : Controller
     {
+        private readonly ApplicationDbContext _dbContext;
         private readonly UserManager<User> _userManager;
         private readonly SignInManager<User> _signInManager;
         private readonly ILogger<ManageController> _logger;
 
         public ManageController(
+        ApplicationDbContext dbContext,
         UserManager<User> userManager,
         SignInManager<User> signInManager,
         ILogger<ManageController> logger)
         {
+            _dbContext = dbContext;
             _userManager = userManager;
             _signInManager = signInManager;
             _logger = logger;
@@ -33,6 +39,7 @@ namespace AutoAllegro.Controllers
         {
             ViewData["StatusMessage"] =
                 message == ManageMessageId.ChangePasswordSuccess ? "Twoje hasło zostało zmienione."
+                : message == ManageMessageId.ChangeAllegroSettings ? "Dane dostępowe do Allegro zostały zmienione pomyślnie.";
                 : message == ManageMessageId.Error ? "Wystąpił błąd."
                 : string.Empty;
 
@@ -47,16 +54,45 @@ namespace AutoAllegro.Controllers
 
         // GET: /Manage/AllegroSettings
         [HttpGet]
-        public IActionResult AllegroSettings() 
+        public async Task<IActionResult> AllegroSettings() 
         {
-            return View();
+            string userId = _userManager.GetUserId(User);
+            var user = await _dbContext.Users.FirstOrDefaultAsync(t => t.Id == userId);
+            if (user != null)
+            {
+                var model = new AllegroSettingsViewModel
+                {
+                    Login = user.AllegroUserName ?? string.Empty,
+                    HashPassword = user.AllegroHashedPass ?? string.Empty,
+                    ApiKey = user.AllegroKey ?? string.Empty
+                };
+
+                return View(model);
+            }
+            return RedirectToAction(nameof(Index), new { Message = ManageMessageId.Error });
         }
 
         // POST: /Manage/AllegroSettings
         [HttpPost]
-        public IActionResult AllegroSettings(AllegroSettingsViewModel model)
+        public async Task<IActionResult> AllegroSettings(AllegroSettingsViewModel model)
         {
-            return RedirectToAction(nameof(Index));
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            string userId = _userManager.GetUserId(User);
+            var user = await _dbContext.Users.FirstOrDefaultAsync(t => t.Id == userId);
+            if (user != null)
+            {
+                user.AllegroHashedPass = model.HashPassword;
+                user.AllegroKey = model.ApiKey;
+                user.AllegroUserName = model.Login;
+                await _dbContext.SaveChangesAsync();
+                _logger.LogInformation("User changed allegro settings successfully.");
+                return RedirectToAction(nameof(Index), new { Message = ManageMessageId.ChangeAllegroSettings });
+            }
+            return RedirectToAction(nameof(Index), new { Message = ManageMessageId.Error });
         }
 
         //
@@ -107,7 +143,8 @@ namespace AutoAllegro.Controllers
         public enum ManageMessageId
         {
             ChangePasswordSuccess,
-            Error
+            Error,
+            ChangeAllegroSettings
         }
 
         private Task<User> GetCurrentUserAsync()
