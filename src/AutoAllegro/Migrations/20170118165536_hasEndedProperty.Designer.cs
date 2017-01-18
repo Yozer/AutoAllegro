cdea11febb47bea0852147ce8ba3e42b7e6e3bf3
Dominik Baran <dominik.baran7@gmail.com>
2017-01-18 18:25:36 +0100

    Added migration for hasEnded property.

--- src/AutoAllegro/Migrations/20170118165536_hasEndedProperty.Designer.cs
+++ src/AutoAllegro/Migrations/20170118165536_hasEndedProperty.Designer.cs
@@ -9,9 +9,10 @@ using AutoAllegro.Models;
 namespace AutoAllegro.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170118165536_hasEndedProperty")]
+    partial class hasEndedProperty
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.0-rtm-22752")
---
+++ src/AutoAllegro/Migrations/20170118165536_hasEndedProperty.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AutoAllegro.Migrations
+{
+    public partial class hasEndedProperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "HasEnded",
+                table: "Auctions",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HasEnded",
+                table: "Auctions");
+        }
+    }
+}
