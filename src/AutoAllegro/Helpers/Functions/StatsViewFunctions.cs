b44359a7fe9389d7cd2cffffb9669ff0a1a35b1c
Damian Pasek <damianpasek@gmail.com>
2017-02-12 00:17:06 +0100

    Add date format parameter for FormatList helper function

--- src/AutoAllegro/Helpers/Functions/StatsViewFunctions.cs
+++ src/AutoAllegro/Helpers/Functions/StatsViewFunctions.cs
@@ -5,13 +5,13 @@ namespace AutoAllegro.Helpers.Functions
 {
     public class StatsFunctions
     {
-        public static string FormatList(List<DateTime> list)
+        public static string FormatList(List<DateTime> list, string dateFormat = "MM.yyyy")
         {
             string returnList = "";
 
             foreach (var element in list) 
             {
-                returnList += string.Format("{0}{1}{2}", "\"", element.ToString("MM.yyyy"), "\",");
+                returnList += string.Format("{0}{1}{2}", "\"", element.ToString(dateFormat), "\",");
             }
 
             return returnList;
